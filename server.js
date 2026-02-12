57b6dec00b73d2c8a8cde1d802473356736ebfca
Mian Ahmad Ghurki <144025913+AhmadRaza630@users.noreply.github.com>
2026-02-12 14:45:31 +0500

    Update server.js

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+
 // MongoDB Connection
 mongoose.connect(process.env.MONGO_URI)
 .then(() => console.log("MongoDB Connected ✅"))
